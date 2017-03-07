9121fed32dbdb4e92c9247243300426272703f03
Ganesh Bhat <Ganesh Bhat>
2017-03-07 15:06:07 +0530

    Changing migration name for migration file

--- server/app/functions/commons.js
+++ server/app/functions/commons.js
@@ -2,9 +2,11 @@
  * Common mesage body
 */
 exports.str = function (results, msg, details) {
+
     return resBody = {
         results: results,
         status: msg,
         details: details
     };
+    
 };
