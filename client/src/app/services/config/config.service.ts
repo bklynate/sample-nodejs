d1d30ae2b29b9dd838c0c5ebaae90d397052fde8
Ganesh Bhat <Ganesh Bhat>
2017-03-08 11:29:58 +0530

    client config changes

--- client/src/app/services/config/config.service.ts
+++ client/src/app/services/config/config.service.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class ConfigService {
 
-    restHost:string = 'http://localhost';
+    restHost:string = 'http://localhost:';
     restPort: string = '9001';
 
     restAPIString: string = this.restHost + this.restPort;
